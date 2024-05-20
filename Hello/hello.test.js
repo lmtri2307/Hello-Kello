4ccde295caf224f4ec053c98d08383dacffdb50e
qhungo0125 <hungngojan25@gmail.com>
2024-05-20 22:59:09 +0700

    update jk

--- Hello/hello.test.js
+++ Hello/hello.test.js
@@ -10,11 +10,11 @@ describe("GET /hello", () => {
     app.get("/hello", hello);
   });
 
-  // it("should return 'Hello World 1111222!'", async () => {
-  //   const res = await request(app).get("/hello");
-  //   expect(res.statusCode).toBe(200);
-  //   expect(res.text).toBe("Hello World 11112223!");
-  // });
+  it("should return 'Hello World 1111222!'", async () => {
+    const res = await request(app).get("/hello");
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe("Hello World 11112223!");
+  });
 
   it("should not return 'Goodbye World'", async () => {
     const res = await request(app).get("/hello");
