399ff3b13ae7d44779724b507bed306071d1a5df
Minh Tri Le <94401753+lmtri2307@users.noreply.github.com>
2024-06-16 23:37:47 +0700

    Update app.js

--- Hello/app.js
+++ Hello/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { hello } = require("./handler");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.get("/", hello);
 
